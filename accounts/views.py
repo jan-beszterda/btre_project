9e7f667002e8203eae6675b59ac072d619e14030
Jan Beszterda <jan.beszterda@gmail.com>
2022-09-20 16:01:10 +0200

    worked on user registration

---
+++ accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username is already taken")
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "Email is already registered")
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, 
+                    first_name=first_name, last_name=last_name)
+                    auth.login(request, user)
+                    messages.success(request, 'Success! You are now registered.')
+                    return redirect('index')
+        else:
+            messages.error(request, "Passwords don't match")
+            return redirect('register')
+    else:
+        return render(request, "accounts/register.html")
+
+def login(request):
+    if request.method == 'POST':
+        # login
+        return
+    else:
+        return render(request, "accounts/login.html")
+
+def logout():
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
